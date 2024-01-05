f2b9cfcd2514a3de5512d993f5ca7659727733c4
stevencoveta <shdegroodt@gmail.com>
2024-01-05 16:29:55 +0100

    m2

--- aave_ingestor.py
+++ aave_ingestor.py
@@ -57,5 +57,51 @@ def transform_results(df, chain):
     return dff
 
 
+import pandas as pd
+import psycopg2
+from psycopg2 import pool
+
+# Adjust the connection parameters based on your PostgreSQL setup
+dsn = 'dbname=postgres user=postgres password=napcat host=13.231.138.2 port=5432'
+
+# Function to create the table if it doesn't exist
+def create_table(conn):
+    with conn.cursor() as cur:
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS public.token_apy (
+                id SERIAL PRIMARY KEY,
+                symbol VARCHAR(50),
+                apy FLOAT,
+                protocol VARCHAR(50),
+                chain VARCHAR(50),
+                last_updated TIMESTAMP
+            )
+        """)
+        conn.commit()
+
+# Function to insert trades into the database
+def insert_dataframe(pool, dataframe):
+    with pool.getconn() as conn:
+        create_table(conn)  # Create the table if it doesn't exist
+
+        with conn.cursor() as cur:
+            for _, row in dataframe.iterrows():
+                query = """
+                    INSERT INTO public.token_apy (symbol, apy, protocol, chain, last_updated)
+                    VALUES (%s, %s, %s, %s, %s)
+                """
+                cur.execute(query, (row['symbol'], row['apy'], row['protocol'], row['chain'], row['last_updated']))
+            conn.commit()
+
+        pool.putconn(conn)
+
+def main(df):
+    connection_pool = psycopg2.pool.SimpleConnectionPool(1, 10, dsn)
+    insert_dataframe(connection_pool, df)
 
 chains = ['protocol-v3','protocol-v2','aave-v2-matic','protocol-v2-avalanche','protocol-v3-arbitrum','protocol-v3-optimism','protocol-v3-polygon','protocol-v3-avalanche']
+
+if __name__ == '__main__':
+    data = fetch_apy('protocol-v3')
+    df = transform_results(data,'protocol-v3')
+    main(df)
